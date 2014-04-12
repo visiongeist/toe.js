e0879a0d5a6c3ff205d28dc7ed4ddca1c136b167
Dan Sawda <dsawada@media.mit.edu>
2014-04-12 15:38:16 -0400

    added drag detection

--- dist/toe.zepto.js
+++ dist/toe.zepto.js
@@ -328,6 +328,60 @@
 }(Zepto, Zepto.toe, this));
 (function ($, touch, window, undefined) {
 
+    var namespace = 'drag', cfg = {
+        distance: 20, // minimum
+        duration: 1200, // maximum
+        direction: 'all'
+    };
+
+    touch.track(namespace, {
+        touchstart: function (event, state, start) {
+            state[namespace] = {
+                finger: start.point.length
+            };
+        },
+        touchmove: function (event, state, move) {
+            var opt = $.extend(cfg, event.data);
+
+            // if another finger was used then increment the amount of fingers used
+            state[namespace].finger = move.point.length > state[namespace].finger ? move.point.length : state[namespace].finger;
+            state[namespace].distance = touch.calc.getDistance(state.start.point[0], move.point[0]);
+            state[namespace].angle = touch.calc.getAngle(state.start.point[0], move.point[0]);
+            state[namespace].direction = touch.calc.getDirection(state[namespace].angle);
+
+            if (state[namespace].distance > opt.distance) {
+                $(event.target).trigger($.Event(namespace, touch.addEventParam(state.start, state[namespace])));
+            }
+
+        },
+        touchend: function (event, state, end) {
+            /*
+            var opt = $.extend(cfg, event.data),
+                duration,
+                distance;
+
+            // calc
+            duration = touch.calc.getDuration(state.start, end);
+            distance = touch.calc.getDistance(state.start.point[0], end.point[0]);
+
+            // check if the swipe was valid
+            if (duration < opt.duration && distance > opt.distance) {
+
+                state[namespace].angle = touch.calc.getAngle(state.start.point[0], end.point[0]);
+                state[namespace].direction = touch.calc.getDirection(state[namespace].angle);
+
+                // fire if the amount of fingers match
+                if (opt.direction === 'all' || state[namespace].direction === opt.direction) {
+                    $(event.target).trigger($.Event(namespace, touch.addEventParam(state.start, state[namespace])));
+                }
+            }
+            */
+        }
+    });
+
+}(Zepto, Zepto.toe, this));
+(function ($, touch, window, undefined) {
+
     var namespace = 'swipe', cfg = {
         distance: 40, // minimum
         duration: 1200, // maximum
